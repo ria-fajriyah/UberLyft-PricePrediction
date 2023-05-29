46779256316e997f60dab8f7febb561cadf21c6a
Nurul Fajriyah Al Maulidiyah <75822692+ria-fajriyah@users.noreply.github.com>
2023-05-29 16:02:50 +0700

    update deployment

--- prediksi_harga.py
+++ prediksi_harga.py
@@ -5,13 +5,23 @@ model = pickle.load(open('prediksi_harga.sav', 'rb'))
 
 st.title("Prediksi Harga Taksi")
 
-hour = st.number_input('Input Jam Pemesanan')
-day = st.number_input('Input Hari Pemesanan')
-distance = st.number_input('Input Jarak Perjalanan')
-short_summary = st.number_input('Input Cuaca saat Pemesanan')
-cab_type = st.number_input('Input Jenis Taksi')
-name = st.number_input('Input Jenis Mobil')
-surge_multiplier = st.number_input('Input Pengganda Tarif')
+# Membagi visualisasi menjadi 2 kolom
+col1, col2 = st.columns(2)
+
+with col1:
+    hour = st.number_input('Input Jam Pemesanan')
+with col2:
+    day = st.number_input('Input Hari Pemesanan')
+with col1:
+    distance = st.number_input('Input Jarak Perjalanan')
+with col2:
+    short_summary = st.number_input('Input Cuaca saat Pemesanan')
+with col1:
+    cab_type = st.number_input('Input Jenis Taksi')
+with col2:
+    name = st.number_input('Input Jenis Mobil')
+with col1:
+    surge_multiplier = st.number_input('Input Pengganda Tarif')
 
 predict = ''
 
