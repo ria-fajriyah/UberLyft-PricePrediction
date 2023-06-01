4f3b072295ed7b6c6c1cce5f27db139475b23456
Nurul Fajriyah Al Maulidiyah <75822692+ria-fajriyah@users.noreply.github.com>
2023-06-01 10:55:14 +0700

    update deployment 2

--- prediksi_harga.py
+++ prediksi_harga.py
@@ -9,19 +9,19 @@ st.title("Prediksi Harga Taksi")
 col1, col2 = st.columns(2)
 
 with col1:
-    hour = st.number_input('Input Jam Pemesanan')
+    hour = st.number_input('input jam pemesanan (0-23)')
 with col2:
-    day = st.number_input('Input Hari Pemesanan')
+    day = st.number_input('input hari pemesanan (1-30)')
 with col1:
-    distance = st.number_input('Input Jarak Perjalanan')
+    distance = st.number_input('input jarak perjalanan dalam mil')
 with col2:
-    short_summary = st.number_input('Input Cuaca saat Pemesanan')
+    short_summary = st.selectbox('input cuaca saat pemesanan', (0 = Clear, 1 = Drizzle, 2 = Foggy, 3 = Light Rain, 4 = Mostly Cloudy, 5 = Overcast, 6 = Party Cloudy, 7 = Possible Drizzle, 8 = Rain))
 with col1:
-    cab_type = st.number_input('Input Jenis Taksi')
+    cab_type = st.selectbox('input jenis taksi', (0 = Lyft, 1 = Uber))
 with col2:
-    name = st.number_input('Input Jenis Mobil')
+    name = st.selectbox('input jenis mobil', (0 = Black, 1 = Black SUV, 2 = Lux, 3 = Lux Black, 4 = Lux Black XL, 5 = Lyft, 6 = Lyft XL, 7 = Shared, 8 = Taxi, 9 = UberPool, 10 = UberX, 11 = UberXL, 12 = WAF))
 with col1:
-    surge_multiplier = st.number_input('Input Pengganda Tarif')
+    surge_multiplier = st.selectbox('input pengganda tarif', (1.00, 1.25, 1.50, 1.75, 2.00, 2.50, 3.00 ))
 
 predict = ''
 
